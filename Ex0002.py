fc1b61ff7d9693bc5484efad7338c36920a82d5a
Lucas Silva <lucknroll66@outlook.com>
2024-01-04 11:07:22 -0300

    Update Ex0002.py

--- Ex0002.py
+++ Ex0002.py
@@ -1,2 +1,2 @@
 nome = input('Qual seu nome?')
-print('É um prazer te conhecer {}!'.format(nome))   #O .format vai formatar o nome para caber no espaço definido pelas chaves
+print('É um prazer te conhecer, {}!'.format(nome))   #O .format vai formatar o nome para caber no espaço definido pelas chaves
