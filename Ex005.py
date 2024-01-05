afaf6735ca5f020736e3c00a0cfbe9e8aafea396
Lucas Silva <lucknroll66@outlook.com>
2024-01-05 11:15:24 -0300

    Adicionei mais exercícios

---
+++ Ex005.py
@@ -0,0 +1,8 @@
+variavel = input('Digite algo:')
+
+print(type(variavel))
+print(variavel.isnumeric())
+print(variavel.isalpha())
+print(variavel.isalnum())
+print(variavel.islower())
+print(variavel.isupper())
