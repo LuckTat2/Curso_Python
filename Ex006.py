e767adbfe0a31370dfdce14a1593379fdb241a49
Lucas Silva <lucknroll66@outlook.com>
2024-01-11 12:02:41 -0300

    Adicionei mais exercícios

---
+++ Ex006.py
@@ -0,0 +1,4 @@
+valor = int(input('Digite um valor: '))
+ant = valor - 1
+suc = valor + 1
+print('Analisando o valor {}, seu antecessor é {} e seu sucessor é {}'.format(valor, ant, suc))
---
+++ Ex007.py
@@ -0,0 +1,7 @@
+valor = int(input('Digite um valor: '))
+dobro = valor * 2
+triplo = valor * 3
+raiz_quadrada = valor ** (1/2)
+print('O dobro de {} é {}'.format(valor,dobro))
+print('O triplo de {} é {}. \nA raiz quadrada de {} é {}'.format(valor,triplo,valor,raiz_quadrada))
+
