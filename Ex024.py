6155b6e11964f1c68667188d39e74290d421e1c4
Lucas Silva <lucknroll66@outlook.com>
2025-01-21 11:14:59 -0300

    Update Ex024.py

--- Ex024.py
+++ Ex024.py
@@ -1,10 +1,10 @@
 num = int(input("Digite um número positivo: "))
 
 if num > 0:
-    unidade = num % 10
-    dezena = num % 100 // 10
-    centena = num % 1000 // 100
-    milhar = num % 10000 // 1000
+    unidade = num // 1 % 10
+    dezena = num // 10 % 10
+    centena = num // 100 % 10
+    milhar = num // 1000 % 10
 
     print("O número possui {} unidades, {} dezenas, {} centenas e {} milhares".format(unidade, dezena, centena, milhar))
     
