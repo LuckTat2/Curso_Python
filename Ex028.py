ba86b57394e1b152a8ff2edc2ebd9a80d0396c95
Lucas Silva <lucknroll66@outlook.com>
2025-01-24 10:49:09 -0300

    Create Ex028.py

---
+++ Ex028.py
@@ -0,0 +1,5 @@
+nome = str(input("Digite seu nome completo: ")).strip()
+nomeDividido = nome.split()
+print("Muito prazer em te conhecer!")
+print("Seu primeiro nome é {}".format(nomeDividido[0]))
+print("Seu último nome é {}".format(nomeDividido[-1]))
