b92d3336895049d0e148f04af98b64c8d6441a4b
Lucas Silva <lucknroll66@outlook.com>
2025-02-05 12:02:37 -0300

    Update Ex035.py

--- Ex035.py
+++ Ex035.py
@@ -1,3 +1,22 @@
-n1 = float(input("Digite o primeiro valor: "))
-n2 = float(input("Digite o segundo valor: "))
-n3 = float(input("Digite o terceiro valor: "))
+n1 = int(input("Digite o primeiro valor: "))
+n2 = int(input("Digite o segundo valor: "))
+n3 = int(input("Digite o terceiro valor: "))
+
+#Menor
+menor = n1
+
+if n2 < n1 and n2 < n3:
+    menor = n2
+if n3 < n1 and n3 < n2:
+    menor = n3
+
+#Maior
+maior = n1
+
+if n2 > n1 and n2 > n3:
+    maior = n2
+if n3 > n1 and n3 > n2:
+    maior = n3
+
+print("O maior valor digitado foi {}".format(maior))
+print("O menor valor digitado foi {}".format(menor))
